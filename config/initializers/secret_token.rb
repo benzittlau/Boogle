bf4019e1bca3fd2bd9b444cce474eae05f7f876d
Ben Zittlau <ben@zittlau.ca>
2011-04-22 16:09:57 -0600

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boogle::Application.config.secret_token = '3f6d60c8f041cec2291736fd04282fc7e4074c51cf323e8597e70eab0fad0ec6c25716b17434fd8cf48924ec36caad0e23c0cf690633fb11491fd193d1d922d8'
